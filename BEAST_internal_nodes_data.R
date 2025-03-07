714977a852d036024dda043389497616be91f8e9
Nicholas Ruffolo <nruffolo5@g.ucla.edu>
2025-03-06 16:39:44 -0800

    Plotting coefficient densities at internal nodes

--- BEAST_internal_nodes_data.R
+++ BEAST_internal_nodes_data.R
@@ -1,5 +1,5 @@
 setwd("~/tanager_spec")
-load("C:\\Users\\batra\\Documents\\tanager_spec\\tanager_spec\\BEAST.trees.subset.RData")
+load("tanager_spec/BEAST.trees.subset.RData")
 library(splines2)
 library(ggplot2)
 library(tidyverse)
@@ -75,7 +75,7 @@ x$rescaled_reflectance[x$rescaled_reflectance < 0] <- 0
 x <- x %>% mutate(wl = 300:700)
 
 # Plots each predicted reflectance curve from the BEAST runs
-x %>% group_by(x$run) %>% ggplot(aes(x = wl, y = rescaled_reflectance)) + geom_point(alpha = 0.5, size = 0.75, color = as.character(rep(y$color, each = 401))) + theme() + theme_bw() + stat_wl_strip(ymin = -20, ymax = -19.5) + scale_fill_identity() + labs(x = "Wavelength (nm)", y = "Reflectance (%)", title = "Root Reflectance Predicted by BEAST")
+x %>% group_by(x$run) %>% ggplot(aes(x = wl, y = rescaled_reflectance)) + geom_point(alpha = 0.25, size = 0.75, color = as.character(rep(y$color, each = 401))) + theme() + theme_bw() + stat_wl_strip(ymin = -1.5, ymax = -0.5) + scale_fill_identity() + labs(x = "Wavelength (nm)", y = "Reflectance (%)", title = "Root Reflectance Predicted by BEAST")
 
 
 
@@ -120,9 +120,113 @@ r <- r %>% rowwise() %>% filter(class(internal_node_coefs) == "list") %>% ungrou
 a <- a %>% left_join(r, by = "join_list") %>% select(-c(nodes_list.y, join_list)) %>% rename(nodes_list = nodes_list.x)
 
 
+# test case with a specific node
+
+test.node <- a %>% filter(nodes_list == 1)
+
+test.node <- test.node %>% rowwise() %>% mutate(intercept_coef = unlist(internal_node_coefs)[1]) %>% ungroup()
+
+test.node <- test.node %>% rowwise() %>% mutate(spline_coefs = list(unlist(internal_node_coefs)[-1])) %>% ungroup()
+
+test.node <- test.node %>% select(-internal_node_coefs)
+
+test.node <- test.node %>% rowwise() %>% mutate(reflectance = list(bspline.matrix %*% matrix(unlist(spline_coefs), ncol = 1))) %>% ungroup()
+
+test.node <- test.node %>% rowwise() %>% mutate(rescaled_reflectance = list(unlist(reflectance) + intercept_coef)) %>% ungroup()
+
+test.node <- test.node %>% rowwise() %>% mutate(test = list(procspec(as.rspec(data.frame(wl = 300:700, reflectance = unlist(rescaled_reflectance))), fixneg = "zero")))
+
+test.node <- test.node %>% rowwise() %>% mutate(color = spec2rgb(data.frame(test)))
+
+
+# Creates a separate tibble that unlists the rescaled_reflectance (stored as a singular vector)
+plot.test.node <- test.node %>% unnest(cols = rescaled_reflectance)
+
+# Manually 'procspec' our rescaled_reflectance
+plot.test.node$rescaled_reflectance[plot.test.node$rescaled_reflectance < 0] <- 0
+
+# Adds a wavelength column
+plot.test.node <- plot.test.node %>% mutate(wl = rep(300:700, 100))
+
+# Plots each predicted reflectance curve from the BEAST runs
+plot.test.node %>% rowwise() %>% ggplot(aes(x = wl, y = rescaled_reflectance)) + geom_point(alpha = 0.25, size = 0.75, pch = 19, color = as.character(rep(y$color, each = 401))) + theme() + theme_bw() + stat_wl_strip(ymin = -1.5, ymax = -0.5) + scale_fill_identity() + labs(x = "Wavelength (nm)", y = "Reflectance (%)", title = "Node 53 Reflectance Predicted by BEAST")
+
+# function to plot all nodes
+
+ASR_plotter <- function(dataframe, nodes){
+  
+  if (sum(names(dataframe) == "nodes_list") == 0){
+    stop("Dataframe needs a column titled 'nodes_list'.")
+  }
+  
+  if (sum(names(dataframe) == "internal_node_coefs") == 0){
+    stop("Dataframe needs a column titled 'internal_node_coefs'.")
+  }
+  
+  if (class(dataframe$internal_node_coefs) != "list"){
+    stop("Spline coefficient column needs to be a list.")
+  }
+    
+  if (length(nodes) == 1){
+    
+    
+    test.node <- dataframe %>% filter(nodes_list == nodes)
+    
+    test.node <- test.node %>% rowwise() %>% mutate(intercept_coef = unlist(internal_node_coefs)[1]) %>% ungroup()
+    
+    test.node <- test.node %>% rowwise() %>% mutate(spline_coefs = list(unlist(internal_node_coefs)[-1])) %>% ungroup()
+    
+    test.node <- test.node %>% select(-internal_node_coefs)
+    
+    test.node <- test.node %>% rowwise() %>% mutate(reflectance = list(bspline.matrix %*% matrix(unlist(spline_coefs), ncol = 1))) %>% ungroup()
+    
+    test.node <- test.node %>% rowwise() %>% mutate(rescaled_reflectance = list(unlist(reflectance) + intercept_coef)) %>% ungroup()
+    
+    test.node <- test.node %>% rowwise() %>% mutate(test = list(procspec(as.rspec(data.frame(wl = 300:700, reflectance = unlist(rescaled_reflectance))), fixneg = "zero")))
+    
+    test.node <- test.node %>% rowwise() %>% mutate(color = spec2rgb(data.frame(test)))
+    
+    
+    # Creates a separate tibble that unlists the rescaled_reflectance (stored as a singular vector)
+    plot.test.node <- test.node %>% unnest(cols = rescaled_reflectance)
+    
+    # Manually 'procspec' our rescaled_reflectance
+    plot.test.node$rescaled_reflectance[plot.test.node$rescaled_reflectance < 0] <- 0
+    
+    # Adds a wavelength column
+    plot.test.node <- plot.test.node %>% mutate(wl = rep(300:700, 100))
+    
+    # Plots each predicted reflectance curve from the BEAST runs
+    plot.test.node %>% rowwise() %>% ggplot(aes(x = wl, y = rescaled_reflectance)) + geom_point(alpha = 0.25, size = 0.75, pch = 19, color = as.character(rep(y$color, each = 401))) + theme() + theme_bw() + stat_wl_strip(ymin = -1.5, ymax = -0.5) + scale_fill_identity() + labs(x = "Wavelength (nm)", y = "Reflectance (%)", title = paste("Node", nodes, "Reflectance Predicted by BEAST"))
+    
+  }
+  
+  
+}
 
 
 
+# Spline coefficient densities at each node for 100 runs
+
+test <- a %>% unnest(cols = internal_node_coefs)
+
+test <- test %>% mutate(coef_name = rep(c("Int.", "1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20","21","22","23","24","25","26","27","28","29","30","31","32","33","34","35","36","37","38"), 4900))
+
+test2 <- test %>% filter(coef_name == "Int." & nodes_internal == 53) %>%  mutate(join_list = 1:length(coef_name))
+
+test2 %>% ggplot(aes(x = unlist(internal_node_coefs))) + geom_density(fill = "blue")
+
+test3 <- test %>% filter(coef_name == "Int." & nodes_internal == 54) %>% mutate(join_list = 1:length(coef_name))
+
+test3 %>% ggplot(aes(x = unlist(internal_node_coefs))) + geom_density(fill = "red")
+
+test4 <- test2 %>% left_join(test3, by = "join_list")
+
+test4 %>% ggplot() + geom_density(aes(x = unlist(internal_node_coefs.x)), fill = "black", alpha = 0.5) + geom_density(aes(x = unlist(internal_node_coefs.y)), fill = "yellow2", alpha = 0.5) + guides(fill = guide_legend(title = "Node")) + theme() + scale_fill_manual(values = c("53", "54"))
+  
+test5 <- test4 %>% pivot_longer(cols = c(internal_node_coefs.x, internal_node_coefs.y))
+
+test5 %>% ggplot(aes(x = unlist(value), fill = name)) + geom_density(alpha = 0.25) + scale_fill_manual(labels = c(53,54), name = "Node", values = c("black", "yellow2"))
 #####
 
 # Numerical values in annotation list corresponding to the internal nodes (not including tip labels) for our tanager tree
